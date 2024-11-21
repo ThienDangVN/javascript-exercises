56391eb6404ed7e3433227044217e80b37761b83
TN <thiendang01031994@gmail.com>
2024-11-21 17:17:35 -0500

    WORKING EXCERSISE
    Logic works for all unit tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...theArgs) {
+
+    for (const e of theArgs) {
+        
+        // get initial index 
+        let indexOfInputArg = array.indexOf(e);
+
+        // stops when indexOf() returns -1 (not found)
+        while(indexOfInputArg !== -1) {
+            array.splice(indexOfInputArg, 1);
+
+            // after removal at the firs found index, start a new index search that matches input arg
+            // at the current index
+            indexOfInputArg = array.indexOf(e, indexOfInputArg);
+        }
+        console.log(array);
+        // let indexOfObj = array.indexOf(sameNum);
+        // array.splice(sameNum, 1);
+    }
+    
+    return array;
+
+}
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
